d4cb3257a3187f212556c9e4d2735103a11099c4
Christine Cua <cloudycua@gmail.com>
2019-02-14 22:36:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function BoardMember (name, homeState, training) {
   };
   this.sayHi = function() {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-  }
+  };
 }
