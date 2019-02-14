64407cc7b115db95da70b4609b8eb8cb939243e9
Christine Cua <cloudycua@gmail.com>
2019-02-14 22:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 // doCharity — returns I like to help people.
 // releasePressStatement — returns You will see great things from Scuber.
 // sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+
+function boardMember (name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
